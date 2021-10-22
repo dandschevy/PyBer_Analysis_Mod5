d2f88917f2b19f04f9ea8ffba963a62f6e4c7c58
dandschevy <dandschevy@hotmail.com>
2021-10-21 18:12:41 -0700

    adding PyBer.ipynb file and Resources folder

---
+++ matplotlib_practice.py
@@ -0,0 +1,109 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "67d1dd71-fb3e-4151-86ce-342e34986e19",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%matplotlib inline"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ea64f9ab-a0b1-44ff-8467-3cefe02a5c11",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#import dependencies\n",
+    "import matplotlib.pyplot as plt"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bf8169d4-6e34-4ef8-ba2b-6360d09afdce",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Set the x-axis to a list of strings for each month.\n",
+    "x_axis = [\"Jan\", \"Feb\", \"Mar\", \"April\", \"May\", \"June\", \"July\", \"Aug\", \"Sept\", \"Oct\", \"Nov\", \"Dec\"]"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "f223fa54-7101-4e9c-9838-68ad81b78aad",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Set the y-axis to a list of floats as the total fare in US dollars accumulated for each month.\n",
+    "y_axis = [10.02, 23.24, 39.20, 35.42, 32.34, 27.04, 43.82, 10.56, 11.85, 27.90, 20.71, 20.09]"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1b140d17-80ae-42c8-a108-c059ee3aaf8d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#create the plot\n",
+    "plt.plot(x_axis, y_axis)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bd7aeb76-d043-4694-8840-55ce09b85c55",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ee0540ee-d7d9-464b-9907-947e48b4565d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a16d1104-ba46-4cdb-8849-95c4ad11410c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d4f97e45-dbd2-4d16-a763-846a73d52727",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "de0d0d8d-5615-421b-894c-d9ab16511d57",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
